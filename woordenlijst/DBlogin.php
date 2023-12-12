fafa5a4ffa0e1bb63ac63ab747194b8e977e62a2
partypooper123456 <117356293+partypooper123456@users.noreply.github.com>
2023-12-12 11:42:46 +0100

    Update DBlogin.php

--- woordenlijst/DBlogin.php
+++ woordenlijst/DBlogin.php
@@ -1,13 +1,17 @@
 <?php
-// Verbind met de database
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "VOP";
+// DB Configuration
+$servername = "62.72.37.103";
+$username = "u556675066_root";
+$password = "Jvw48roeb!";
+$dbname = "u556675066_vop";
+
+// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Controleer de verbinding
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+    die("Connection failed: " . $conn->connect_error);}
+
+else print "Connection succeeded";
+
 ?>

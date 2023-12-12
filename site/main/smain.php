e1521aedb2baa3182de0cb4c69837b69fbee5bde
Wolfpl2001 <112547123+Wolfpl2001@users.noreply.github.com>
2023-12-12 11:43:31 +0100

    main Update

--- site/main/smain.php
+++ site/main/smain.php
@@ -3,18 +3,43 @@
 <meta content="width=device-width">
 
 <head>
+<meta content="width=device-width" name="viewport">
   <link rel="stylesheet" href="../CSS/main.css">
   <link rel="stylesheet" href="../CSS/nav.css">
   <link rel="icon" type="image/x-icon" href="../img/icon.ico">
   <title>GildeDEVops</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
-<?php include "../nav/snav.php"?>
+<!--nav Bar -->
+<?php include "../cfg/nav/snav.php"?>
+<!--nav Bar -->
 <body>
   <section>
-    <!-- Main menu line on top of site -->
+    <!-- Main menu/line on top of site -->
     <div class="main-menu">
       <div class="Line"></div>
+    </div>
+    <!-- Middle site information -->
+    <div class ="bgTheamas">
+      <table>
+      <tr>
+      <td>
+            <a href="https://stackoverflow.com/questions/3337914/how-can-i-make-a-link-from-a-td-table-cell">
+              Test1
+            </a>
+          </td>
+        <td>Test2</td>
+        <td>Test3</td>
+        <td>Test4</td>
+        <td>Test5</td>
+      </tr>
+      <tr>
+      <td>Test6</td>
+        <td>Test7</td>
+        <td>Test8</td>
+        <td>Test9</td>
+        <td>Test10</td>
+      </tr>
+    </table>
     </div>
   </section>
   <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>

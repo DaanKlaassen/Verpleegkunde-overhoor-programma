39bd1cfea535ff4fb732fa7c262f29b896934cc0
partypooper123456 <117356293+partypooper123456@users.noreply.github.com>
2023-12-14 13:27:07 +0100

    line bottom border

---
+++ site/main/profile.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta content="width=device-width">
+
+<head>
+<meta content="width=device-width" name="viewport">
+  <link rel="stylesheet" href="../CSS/profile.css">
+  <link rel="stylesheet" href="../CSS/nav.css">
+  <link rel="icon" type="image/x-icon" href="../img/icon.ico">
+  <title>GildeDEVops</title>
+</head>
+<!--nav Bar -->
+<?php include "../cfg/nav/snav.php"?>
+<!--nav Bar -->
+<body>
+  <section>
+    <!-- Main menu/line on top of site -->
+    <div class="main-menu">
+      <div class="Line"></div>
+    </div>
+    <!-- Middle site information -->
+    <div class="profileback">
+    
+    
+    </div>
+  </section>
+  <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+</body>
+
+</html>

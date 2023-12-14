1d28c51da6bfce532e5f9a8056b82cfc46ae9529
partypooper123456 <117356293+partypooper123456@users.noreply.github.com>
2023-12-14 14:45:42 +0100

    profiel pagina deel 1 (to be continued)

--- site/main/profile.php
+++ site/main/profile.php
@@ -14,14 +14,40 @@
 <!--nav Bar -->
 <body>
   <section>
-    <!-- Main menu/line on top of site -->
+    <!-- Main menu and line on top of site -->
     <div class="main-menu">
-      <div class="Line"></div>
     </div>
     <!-- Middle site information -->
     <div class="profileback">
+
+        <div class="userinfotop">
+
+            <div class="profilepicture">
+                <img src="../photos/profilepicture1.jpg" alt="profilepicture" class="profilepicture">
+            </div>
+
+            <div class="userinfoname">
+                <h1 class="userinfoname">Mahan Headrex</h1>
+            </div>
+
+        </div>
+
+        <div class="panel1">
+
+            <div class="streakbox">
+            <ion-icon name="flash"></ion-icon>
+            </div>
+            <div class="leaderboardbox">
+            <ion-icon name="trophy"></ion-icon>
+            </div>
+            <div class="puntenbox">
+            <ion-icon name="logo-bitcoin"></ion-icon>
+            </div>
+
+        </div>
+
     
-    
+
     </div>
   </section>
   <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
